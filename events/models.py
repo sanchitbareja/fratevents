b5b71ee5680db2fc774c134edb0afcffa4065514
Sanchit Bareja <sanchitbareja@gmail.com>
2012-10-18 11:55:32 -0700

    added a hidden halloween type in choices aviailable for eventTypes

--- events/models.py
+++ events/models.py
@@ -17,6 +17,23 @@ TYPE_OF_EVENT_CHOICES = (
 	('Other','Others')
 	)
 
+TYPE_OF_EVENT_CHOICES_HIDDEN = (
+	('Parties', 'Parties'),
+	('Concerts', 'Concerts'),
+	('Greeklife', 'Greeklife'),
+	('Sports', 'Sports'),
+	('Philanthropy', 'Philanthropy'),
+	('Performances', 'Performances'),
+	('Conferences', 'Conferences'),
+	('Movies', 'Movies'),
+	('Food', 'Food'),
+	('Green','Green'),
+	('Celebrity','Celebrity'),
+	('Exhibitions','Exhibitions'),
+	('Other','Others'),
+	('halloween','halloween'),
+	)
+
 class Location(models.Model):
 	name = models.CharField(max_length=300)
 	lat = models.DecimalField(max_digits=13, decimal_places=10)
@@ -33,7 +50,7 @@ class Event(models.Model):
 	club = models.ForeignKey(Club)
 	location = models.ForeignKey(Location, blank = True, null = True)
 	image = models.URLField(blank = True, null = True)
-	typeOfEvent = models.CharField(max_length = 256, choices = TYPE_OF_EVENT_CHOICES, null=True, blank = True)
+	typeOfEvent = models.CharField(max_length = 256, choices = TYPE_OF_EVENT_CHOICES_HIDDEN, null=True, blank = True)
 
 	def __unicode__(self):
 		return self.title+", "+str(self.startTime)
