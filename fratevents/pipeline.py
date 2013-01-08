e5a635e057a9b1c4e6f2f3d8a23b00764bd7e06f
Sanchit Bareja <sanchitbareja@gmail.com>
2013-01-08 12:41:42 +0800

    wrong function name

--- fratevents/pipeline.py
+++ fratevents/pipeline.py
@@ -1,5 +1,6 @@
-def get_user_profile_pic(backend, details, response, social_user, uid,\
-                    user, *args, **kwargs):
+from social_auth.backends.facebook import FacebookBackend
+
+def get_user_profile_pic(backend, details, response, social_user, uid, user, *args, **kwargs):
     url = None
     print response
     print backend
@@ -7,4 +8,5 @@ def get_user_profile_pic(backend, details, response, social_user, uid,\
         url = "http://graph.facebook.com/%s/picture?type=large" % response['id']
  
     if url:
+        print "url"
         print url
