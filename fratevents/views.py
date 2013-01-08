f34e0f8ef45b3b9f3d1bf67f4a687269bb396c60
Sanchit Bareja <sanchitbareja@gmail.com>
2013-01-08 12:02:58 +0800

    modifying the social_auth pipeline by getting the custom profile pic

--- fratevents/views.py
+++ fratevents/views.py
@@ -10,16 +10,16 @@ from datetime import datetime, timedelta, time
 from fratevents.settings import EVENT_MASTERS
 
 def main(request):
-	return render_to_response('index.html', context_instance=RequestContext(request))
+    return render_to_response('index.html', context_instance=RequestContext(request))
 
 def feedbackForm(request):
-	results = {'success':False}
-	if(request.method == u'POST'):
-		POST = request.POST
-		send_mail('[CalHaps] Someone just left you a feedback!', POST['feedback']+" REPLY TO EMAIL: "+POST['replyToEmail'], 'caleventsinfo@gmail.com', EVENT_MASTERS, fail_silently=False)
-		results['success'] = True
-	json_results = simplejson.dumps(results)
-	return HttpResponse(json_results, mimetype='application/json')
+    results = {'success':False}
+    if(request.method == u'POST'):
+        POST = request.POST
+        send_mail('[CalHaps] Someone just left you a feedback!', POST['feedback']+" REPLY TO EMAIL: "+POST['replyToEmail'], 'caleventsinfo@gmail.com', EVENT_MASTERS, fail_silently=False)
+        results['success'] = True
+    json_results = simplejson.dumps(results)
+    return HttpResponse(json_results, mimetype='application/json')
 
 
 #TESTING FACEBOOK INTEGRATION
@@ -59,7 +59,7 @@ def logout(request):
     return HttpResponseRedirect('/')
 
 def form(request):
-	print "form1"
+    print "form1"
     if request.method == 'POST' and request.POST.get('username'):
         name = setting('SOCIAL_AUTH_PARTIAL_PIPELINE_KEY', 'partial_pipeline')
         request.session['saved_username'] = request.POST['username']
@@ -68,7 +68,7 @@ def form(request):
     return render_to_response('form.html', {}, RequestContext(request))
 
 def form2(request):
-	print "form2"
+    print "form2"
     if request.method == 'POST' and request.POST.get('first_name'):
         request.session['saved_first_name'] = request.POST['first_name']
         name = setting('SOCIAL_AUTH_PARTIAL_PIPELINE_KEY', 'partial_pipeline')
