e00c7decedee2c3f7efa2c9768985395a64c923a
Sanchit Bareja <sanchitbareja@gmail.com>
2013-01-08 12:12:11 +0800

    fixed typo

--- fratevents/pipeline.py
+++ fratevents/pipeline.py
@@ -1,7 +1,8 @@
 def get_user_avatar(backend, details, response, social_user, uid,\
                     user, *args, **kwargs):
     url = None
-    print respose
+    print response
+    print backend
     if backend.__class__ == FacebookBackend:
         url = "http://graph.facebook.com/%s/picture?type=large" % response['id']
  
