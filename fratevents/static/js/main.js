8eadc8cc91091046836a25413c209728f2f02d3b
Sanchit Bareja <sanchitbareja@gmail.com>
2013-01-11 17:41:48 +0800

    UI changes

--- fratevents/static/js/main.js
+++ fratevents/static/js/main.js
@@ -179,10 +179,6 @@ function getFilters(){
 	});
 }
 
-function getThanksgiving() {
-	$( "#thanksgiving_banner" ).show( "puff", {}, 200 )
-}
-
 function getFilteredEvents(filter_text){
 	$.ajax({
 		type: "POST",
@@ -274,76 +270,7 @@ function getClubInfo(hostid){
 	});
 }
 
-function setCookie( name, value, expires, path, domain, secure ) {
-	// set time, it's in milliseconds
-	var today = new Date();
-	today.setTime( today.getTime() );
-
-	/*
-	if the expires variable is set, make the correct
-	expires time, the current script below will set
-	it for x number of days, to make it for hours,
-	delete * 24, for minutes, delete * 60 * 24
-	*/
-	if ( expires )
-	{
-	expires = expires * 1000 * 60 * 60 * 24;
-	}
-	var expires_date = new Date( today.getTime() + (expires) );
-
-	document.cookie = name + "=" +escape( value ) +
-	( ( expires ) ? ";expires=" + expires_date.toGMTString() : "" ) +
-	( ( path ) ? ";path=" + path : "" ) +
-	( ( domain ) ? ";domain=" + domain : "" ) +
-	( ( secure ) ? ";secure" : "" );
-}
-
-function getCookie( check_name ) {
-	// first we'll split this cookie up into name/value pairs
-	// note: document.cookie only returns name=value, not the other components
-	var a_all_cookies = document.cookie.split( ';' );
-	var a_temp_cookie = '';
-	var cookie_name = '';
-	var cookie_value = '';
-	var b_cookie_found = false; // set boolean t/f default f
-
-	for ( i = 0; i < a_all_cookies.length; i++ )
-	{
-		// now we'll split apart each name=value pair
-		a_temp_cookie = a_all_cookies[i].split( '=' );
-
-
-		// and trim left/right whitespace while we're at it
-		cookie_name = a_temp_cookie[0].replace(/^\s+|\s+$/g, '');
-
-		// if the extracted name matches passed check_name
-		if ( cookie_name == check_name )
-		{
-			b_cookie_found = true;
-			// we need to handle case where cookie has no value but exists (no = sign, that is):
-			if ( a_temp_cookie.length > 1 )
-			{
-				cookie_value = unescape( a_temp_cookie[1].replace(/^\s+|\s+$/g, '') );
-			}
-			// note that in cases where cookie is initialized but no value, null is returned
-			return cookie_value;
-			break;
-		}
-		a_temp_cookie = null;
-		cookie_name = '';
-	}
-	if ( !b_cookie_found )
-	{
-		return null;
-	}
-}
-
 function loadScript() {
-	if(getCookie('joyride') != 'true'){
-		console.log("joyride cookie is not tere");
-		$(document).joyride();
-		setCookie('joyride','true',365,'/',document.domain,false);
-	}
 	var script = document.createElement("script");
 	script.type = "text/javascript";
 	//import google maps with the callback function as initialize()
@@ -376,14 +303,6 @@ function formatAMPM(date) {
 	month = date.getMonth() + 1;
 	strTime = month+"/"+day+", "+hours + ':' + minutes + ' ' + ampm;
 	return strTime;
-}
-
-function start_tour(){
-	if ( typeof reinit == 'function' ) {
-                reinit();
-    }
-
-    $(document).joyride();
 }
 
 window.onload = loadScript;
