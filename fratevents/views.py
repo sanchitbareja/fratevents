7d22807166f4325a710092759ee95f88a4d9056a
Sanchit Bareja <sanchitbareja@gmail.com>
2013-01-11 18:53:43 +0800

    logout view

--- fratevents/views.py
+++ fratevents/views.py
@@ -9,6 +9,14 @@ import os, time, simplejson
 from datetime import datetime, timedelta, time
 from fratevents.settings import EVENT_MASTERS
 
+#TESTING FACEBOOK INTEGRATION
+from django.contrib.auth import logout as auth_logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.messages.api import get_messages
+from social_auth import __version__ as version
+from social_auth.utils import setting
+
+
 def main(request):
     if request.user.is_authenticated():
         print request.user
@@ -26,22 +34,7 @@ def feedbackForm(request):
     json_results = simplejson.dumps(results)
     return HttpResponse(json_results, mimetype='application/json')
 
-
-#TESTING FACEBOOK INTEGRATION
-from django.contrib.auth import logout as auth_logout
-from django.contrib.auth.decorators import login_required
-from django.contrib.messages.api import get_messages
-from social_auth import __version__ as version
-from social_auth.utils import setting
-
-def home(request):
-    """Home view, displays login mechanism"""
-    if request.user.is_authenticated():
-        return HttpResponseRedirect('done')
-    else:
-        return render_to_response('home.html', {'version': version},
-                                  RequestContext(request))
-
+#facebook integration
 @login_required
 def done(request):
     """Login complete view, displays user data"""
@@ -62,24 +55,3 @@ def logout(request):
     """Logs out user"""
     auth_logout(request)
     return HttpResponseRedirect('/')
-
-def form(request):
-    print "form1"
-    if request.method == 'POST' and request.POST.get('username'):
-        name = setting('SOCIAL_AUTH_PARTIAL_PIPELINE_KEY', 'partial_pipeline')
-        request.session['saved_username'] = request.POST['username']
-        backend = request.session[name]['backend']
-        return redirect('socialauth_complete', backend=backend)
-    return render_to_response('form.html', {}, RequestContext(request))
-
-def form2(request):
-    print "form2"
-    if request.method == 'POST' and request.POST.get('first_name'):
-        request.session['saved_first_name'] = request.POST['first_name']
-        name = setting('SOCIAL_AUTH_PARTIAL_PIPELINE_KEY', 'partial_pipeline')
-        backend = request.session[name]['backend']
-        return redirect('socialauth_complete', backend=backend)
-    return render_to_response('form2.html', {}, RequestContext(request))
-
-def close_login_popup(request):
-    return render_to_response('close_popup.html', {}, RequestContext(request))
