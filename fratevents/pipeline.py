0b0955747f741c39a869b70e85d3b700d96ab063
Sanchit Bareja <sanchitbareja@gmail.com>
2013-02-18 13:01:51 -0800

    saves users profiles to user_profile as a link

--- fratevents/pipeline.py
+++ fratevents/pipeline.py
@@ -5,6 +5,7 @@ from django.core.files import File
 from urllib2 import urlopen
 from userprofile.models import UserProfile
 import time, datetime
+from fratevents.settings import AWS_UPLOAD_DESTINATION
 
 def create_user_profile(backend, details, response, social_user, uid, user, *args, **kwargs):
     # create user profile here.
@@ -21,29 +22,30 @@ def get_user_profile_pic(backend, details, response, social_user, uid, user, *ar
         url = "https://graph.facebook.com/%s/picture?type=large" % response['id']
  
     if url:
-        print "url"
-        print url
         profile_pic = urlopen(url).read()
         fout = open(response['id'], "wb") #filepath is where to save the image
         fout.write(profile_pic)
         fout.close()
         default_storage.save(str(response['id'])+'.jpg',File(open(response['id'],'rwb')))
-    
+        #save profile pic to user profile
+        user = UserProfile.objects.filter(user = user)
+        user.profilePic = AWS_UPLOAD_DESTINATION+str(response['id'])+".jpg"
+        user.save()
 
 def get_user_events(backend, details, response, social_user, uid, user, *args, **kwargs):
     url = None
     if backend.__class__ == FacebookBackend:
         url = "https://graph.facebook.com/"+str(response['id'])+"/events?access_token="+str(response['access_token'])
-        print urlopen(url).read()
+        # print urlopen(url).read()
 
 def get_user_network(backend, details, response, social_user, uid, user, *args, **kwargs):
     url = None
     if backend.__class__ == FacebookBackend:
         url = "https://graph.facebook.com/fql?q=select%20affiliations%20from%20user%20where%20uid="+str(response['id'])+"&access_token="+response['access_token']
-        print urlopen(url).read()
+        # print urlopen(url).read()
 
 def get_user_pages(backend, details, response, social_user, uid, user, *args, **kwargs):
     url = None
     if backend.__class__ == FacebookBackend:
         url = "https://graph.facebook.com/fql?q=select%20page_id%20from%20page_admin%20where%20uid="+str(response['id'])+"&access_token="+response['access_token']
-        print urlopen(url).read()
+        # print urlopen(url).read()
--- userprofile/models.py
+++ userprofile/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
+from fratevents.settings import AWS_UPLOAD_DESTINATION
 
 class UserProfile(models.Model):
     # This field is required.
@@ -9,3 +10,4 @@ class UserProfile(models.Model):
     # Other fields here
     # accepted_eula = models.BooleanField()
     birthday = models.DateField()
+    profilePic = models.TextField(null = True, blank = True, default=AWS_UPLOAD_DESTINATION+"unknown_profile.png")
